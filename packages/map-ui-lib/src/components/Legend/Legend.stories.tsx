b0d1b764131413022bae167654d7ce1d0b11ed15
caesterlein <christopher.esterlein@gmail.com>
2026-03-30 00:49:55 -0600

    storybook build fix

--- packages/map-ui-lib/src/components/Legend/Legend.stories.tsx
+++ packages/map-ui-lib/src/components/Legend/Legend.stories.tsx
@@ -1,5 +1,5 @@
 import type { Meta, StoryObj } from '@storybook/react';
-import { fn } from '@storybook/test';
+import { fn } from 'storybook/test';
 import { Legend } from './Legend';
 import type { LayerConfig } from '../../types';
 
